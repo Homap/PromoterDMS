ff9eeeae8291d61d4a4645c0683d3531f791cdea
homap <hpapoli@gmail.com>
2023-05-31 16:27:54 -0400

    function for fragment design

--- code/oligo_fragments.py
+++ code/oligo_fragments.py
@@ -1,3 +1,4 @@
+import sys
 from Bio.Restriction import *
 from Bio.Seq import Seq
 from revcomp import revcomp
@@ -37,47 +38,228 @@ plasmid = open(sys.argv[2], "r")
 #*********************************************************************************************
 # Definition of functions 
 #*********************************************************************************************
+# We need two functions for primer design: For PCR of wild-type fragments and another for assembly primer design
+def design_primer(fragment, length):
+    """
+    Design primer for each wildtype fragment
+    It slides by one base and produce a new primer pair and assess the best possible pair
+    """
+    fprimer = fragment[0:length]
+    rev_compliment = revcomp(fragment)
+    rprimer = rev_compliment[0:length]
+    return([fprimer, rprimer])
+#*********************************************************************************************
+# Plasmid homology arms
+HO_locus = plasmid_dict['pGGAselect-YFPins'][412:571]
+YFP_locus = plasmid_dict['pGGAselect-YFPins'][658:848]
+#*********************************************************************************************
+# Function to output mutated and wild-type fragments
+def oligo_design(promoter_sequence, fragment_len=150):
+    num_oligo_fragments = round(len(promoter_sequence)/fragment_len)
+    mutated_oligo_dict = {}
+    WT_oligo_dict = {}
+    for oligo in range(1, num_oligo_fragments+1):
+        if oligo == 1:
+            first_fragment = promoter_sequence[0:fragment_len+20]
+            key = "oligo_" + str(oligo)
+            mutated_oligo_dict[key] = first_fragment
+            first_fragment_WT = promoter_sequence[fragment_len:len(promoter_sequence)]
+            WT_oligo_dict[key] = first_fragment_WT
+        elif oligo == num_oligo_fragments:
+            last_fragment = promoter_sequence[((num_oligo_fragments-1)*fragment_len)-20:len(promoter_sequence)]
+            key = "oligo_" + str(oligo)
+            mutated_oligo_dict[key] = last_fragment
+            last_fragment_WT = promoter_sequence[0:((num_oligo_fragments-1)*fragment_len)]
+            WT_oligo_dict[key] = last_fragment_WT
+        else:
+            if num_oligo_fragments > 2:
+                mid_fragment = promoter_sequence[((oligo-1)*fragment_len)-20:(oligo*fragment_len)+20]
+                key = "oligo_" + str(oligo)
+                mutated_oligo_dict[key] = mid_fragment
+                mid_fragment_WT_1 = promoter_sequence[0:(oligo-1)*fragment_len]
+                mid_fragment_WT_2 = promoter_sequence[oligo*fragment_len:len(promoter_sequence)]
+                WT_oligo_dict[key] = [mid_fragment_WT_1, mid_fragment_WT_2]
+            else:
+                pass
+    return([mutated_oligo_dict, WT_oligo_dict])
+
+
+promoter_f = open("../data/promoters/promoter_sequences.fasta", "r")
+promoter_dict = readfasta(promoter_f)
+
+mutated_WT_oligo_dict = {}
+for promoter in promoter_dict.keys():
+    mutated_WT_oligo_dict[promoter] = oligo_design(promoter_dict[promoter])
+
+
+
+fragments_dict = {}
+for promoter in promoter_dict.keys():
+    promoter_seq = promoter_dict[promoter]
+    fragment_list = fragments(promoter_seq)
+    for part in fragment_list:
+        if promoter in fragments_dict.keys():
+            fragments_dict[promoter].append(part)
+        else:
+            fragments_dict[promoter] = [part]
+
+
+
+for promoter in fragments_dict.keys():
+    print(promoter)
+    for part in fragments_dict[promoter]:
+        print(len(part))
+
+
+PFY1 = "AGGAGACGTTACTTTGTTTATATATATTAGTATGTACAATCGCAAAGAAATGGAGTGATGACATGTTGTAGTATTTAGTATGAGGTTACTGTGTGGGAGGTTTTACCATGATTTTTGGCGAGAACACGCCATGAAATGTCTTTGTACGAAACTCATTACCCGCATTAATATTTTTTTTCTTTTTAAAGCTCAGTTGACCCTTTCTCATTCCCTTCTTAAAACAACTGTGTGATCCTTGAGAAAAGATAAATTACATACACAACATAAACCCAACTACGATCGCAAATT"
+
+round(len(PFY1)/150)
+
+len(PFY1)/2
+
+# Define different experiments: 
+# Experiment 1: M1 - W2
+# Experiment 2: W1 - M2
+
+# Wild-type PCR primers for PFY1
+pfy1_f1 = PFY1[0:144]
+pfy1_f2 = PFY1[(144-20):]
+
+PFY1_primers_plus_HO_arm_5 = HO_locus[-30:] + design_primer(pfy1_f1, 20)[0]
+PFY1_primers_plus_HO_arm_3 = design_primer(pfy1_f1, 20)[1]
+PFY1_primers_plus_YFP_arm_5 = design_primer(pfy1_f2, 20)[0]
+PFY1_primers_plus_YFP_arm_3 = (design_primer(pfy1_f2, 20)[1][::-1] + revcomp(YFP_locus, reverse=False)[:30])[::-1]
+
+# Wild-type PCR primers for PFY1
+pfy1_f1 = PFY1[0:144-20]
+pfy1_f2 = PFY1[(145-20-20):]
+
+PFY1_primers_plus_HO_arm_5 = HO_locus[-30:] + design_primer(pfy1_f1, 20)[0]
+PFY1_primers_plus_HO_arm_3 = design_primer(pfy1_f1, 19)[1]
+PFY1_primers_plus_YFP_arm_5 = design_primer(pfy1_f2, 20)[0]
+PFY1_primers_plus_YFP_arm_3 = (design_primer(pfy1_f2, 20)[1][::-1] + revcomp(YFP_locus, reverse=False)[:30])[::-1]
+
+
+
+
+dsr_PFY1 = Dseqrecord(PFY1)
+
+
+VMA7 = "TTTCCAATTTTTGATGCGTTCATACCAAGTTTGCTGTTTTTCTTAGTTATTATCATCATCTTTCCCCTTATTGCTGCTGAAACTTGCGTACGTGCGTACTAAAAAAATTTCACTGCGCTTACGCCGGTAAAGGGAAGTAGGAAAACGAAAGAAAAAAAAAAAATAAAGGCGAACGATAACAGGAAAACCAACGTGAATTGCAAGCACTACATTTATTATATCGAGTTGACAAAGAATTAAGAAAATAGAGCACTACATTTCTATATTCATAGCTAGTCTCACTGACGCATAGTAACTAAATC"
+
+round(len(VMA7)/150)
+len(VMA7)/2
+
+# Write a function for this
+
+# Wild-type PCR primers for PFY1
+vma7_f1 = VMA7[0:(151)]
+vma7_f2 = VMA7[(151-20):]
+
+VMA7_primers_plus_HO_arm_5 = HO_locus[-30:] + design_primer(vma7_f1, 21)[0]
+VMA7_primers_plus_HO_arm_3 = design_primer(vma7_f1, 20)[1]
+VMA7_primers_plus_YFP_arm_5 = design_primer(vma7_f2, 20)[0]
+VMA7_primers_plus_YFP_arm_3 = (design_primer(vma7_f2, 20)[1][::-1] + revcomp(YFP_locus, reverse=False)[:30])[::-1]
+
+# Wild-type PCR primers for PFY1
+pfy1_f1 = PFY1[0:144-20]
+pfy1_f2 = PFY1[(145-20-20):]
+
+
+OST1 = "GTTAACTTTCTGTGTGTACTTTGAAGTAGCAATTCTTATCGTAATTGTACACCGTCTCGCAACTTGTTTAAAATACTGTTAAAGATTGCATTTCAATGCTTTTCCCTTTTTTGCCACCGGATTCCGTTGCGAGTGGAAAAGTGAAAATGACAAAATTCAAAAGAAATCTATCAATATAACAAAAACGCCAAATCATCAAAAATGATCGGTGTTGAAGAGCTTGGCACTCTTAAAGGCGGCTAGTTCATAATGCTATAGGAATTGACTCAAGGAAACGGACTATGTCTTGTACTGAATACTGTCTTCAATTGCCCATAGAATGCGTTAGTGTTACTCTTCTTCGCGAGCAGGGTAAGTATGCGCGTAATGTTTTTTATTTTCTGAAAGGTTCAAAGTATGCAGAACAAATTAATGTTTGCTTTTTATTAAGCAAGCTACTTCTTTGACAAGTACCCGATTGCTTCTTTAGCTCGGAACAAGACGCAAACTACAAAATATTGGTGCTGAAAAA"
+
+num_fragments = round(len(OST1)/150)
+fragment_length = len(OST1)/num_fragments
+# Three fragments, two of 170 bps and one of 171 bp
+# Three fragments are 171+(170*2)
+
+# Wild-type PCR primers for PFY1
+ost1_f1 = OST1[170-30:]
+ost1_f2 = OST1[0:170+30]
+ost1_f3 = OST1[(170*2)-30:]
+ost1_f4 = OST1[:(170*2)+30]
+
+ost1_f1_primer_5 = design_primer(ost1_f1, 20)[0]
+ost1_f1_primer_3 = design_primer(ost1_f1, 20)[1] + YFP_locus[:30]
+
+ost1_f2_primer_5 = HO_locus[-30:] + design_primer(ost1_f2, 20)[0]
+ost1_f2_primer_3 = design_primer(ost1_f2, 20)[1] 
+
+ost1_f3_primer_5 = design_primer(ost1_f3, 20)[0] 
+ost1_f3_primer_3 = design_primer(ost1_f3, 20)[1] + YFP_locus[:30]
+
+ost1_f4_primer_5 = HO_locus[-30:] + design_primer(ost1_f4, 20)[0] 
+ost1_f4_primer_3 = design_primer(ost1_f4, 20)[1] 
+
+primers_plus_HO_arm_5 = HO_locus[-30:] + design_primer(vma7_f1, 20)[0]
+primers_plus_HO_arm_3 = design_primer(vma7_f1, 20)[1]
+primers_plus_YFP_arm_5 = design_primer(vma7_f2, 20)[0]
+primers_plus_YFP_arm_3 = design_primer(vma7_f2, 20)[1] + YFP_locus[:30]
+
+#*********************************************************************************************
 def mutate_promoter(promoter):
     """
     Function to produce 1 mutation to each of three possibile bases
     on the promoter sequence
     """
-    promoter_dict = readfasta(promoter) # read promoter fasta into a dictionary
-    gene_name = list(promoter_dict.keys())[0]
+    #promoter_dict = readfasta(promoter) # read promoter fasta into a dictionary
+    promoter_dict = promoter
     mutated_promoter_dict = {}
-    mutated_promoter_dict[gene_name+"_0"+"_WT"] = promoter_dict[gene_name] # initate the mutated dictionary with the wild type sequence
-    #for position in range(0, len(promoter_dict[gene_name])):
-    for index, item in enumerate(list(promoter_dict[gene_name])):
-        nuc = promoter_dict[gene_name][index]
-        if nuc == 'A':
-            for base in nuc_dict['A']:
-                mutated_seq_key = gene_name + "_" + str(index+1) + "_" + nuc + base
-                promoter_base_list = list(promoter_dict[gene_name])
-                promoter_base_list[index] = base
-                mutated_seq = "".join(promoter_base_list)
-                mutated_promoter_dict[mutated_seq_key] = mutated_seq
-        if nuc == 'T':
-            for base in nuc_dict['T']:
-                mutated_seq_key = gene_name + "_" + str(index+1) + "_" + nuc + base
-                promoter_base_list = list(promoter_dict[gene_name])
-                promoter_base_list[index] = base
-                mutated_seq = "".join(promoter_base_list)
-                mutated_promoter_dict[mutated_seq_key] = mutated_seq
-        if nuc == 'C':
-            for base in nuc_dict['C']:
-                mutated_seq_key = gene_name + "_" + str(index+1) + "_" + nuc + base
-                promoter_base_list = list(promoter_dict[gene_name])
-                promoter_base_list[index] = base
-                mutated_seq = "".join(promoter_base_list)
-                mutated_promoter_dict[mutated_seq_key] = mutated_seq
-        if nuc == 'G':
-            for base in nuc_dict['G']:
-                mutated_seq_key = gene_name + "_" + str(index+1) + "_" + nuc + base
-                promoter_base_list = list(promoter_dict[gene_name])
-                promoter_base_list[index] = base
-                mutated_seq = "".join(promoter_base_list)
-                mutated_promoter_dict[mutated_seq_key] = mutated_seq
+    for gene_name in promoter_dict.keys():
+        #for position in range(0, len(promoter_dict[gene_name])):
+        for index, item in enumerate(list(promoter_dict[gene_name])):
+            nuc = promoter_dict[gene_name][index]
+            if nuc == 'A':
+                for base in nuc_dict['A']:
+                    mutated_seq_key = gene_name + "_" + str(index+1) + "_" + nuc + base
+                    promoter_base_list = list(promoter_dict[gene_name])
+                    promoter_base_list[index] = base
+                    mutated_seq = "".join(promoter_base_list)
+                    mutated_promoter_dict[mutated_seq_key] = mutated_seq
+            if nuc == 'T':
+                for base in nuc_dict['T']:
+                    mutated_seq_key = gene_name + "_" + str(index+1) + "_" + nuc + base
+                    promoter_base_list = list(promoter_dict[gene_name])
+                    promoter_base_list[index] = base
+                    mutated_seq = "".join(promoter_base_list)
+                    mutated_promoter_dict[mutated_seq_key] = mutated_seq
+            if nuc == 'C':
+                for base in nuc_dict['C']:
+                    mutated_seq_key = gene_name + "_" + str(index+1) + "_" + nuc + base
+                    promoter_base_list = list(promoter_dict[gene_name])
+                    promoter_base_list[index] = base
+                    mutated_seq = "".join(promoter_base_list)
+                    mutated_promoter_dict[mutated_seq_key] = mutated_seq
+            if nuc == 'G':
+                for base in nuc_dict['G']:
+                    mutated_seq_key = gene_name + "_" + str(index+1) + "_" + nuc + base
+                    promoter_base_list = list(promoter_dict[gene_name])
+                    promoter_base_list[index] = base
+                    mutated_seq = "".join(promoter_base_list)
+                    mutated_promoter_dict[mutated_seq_key] = mutated_seq
     return(mutated_promoter_dict)
+
+# Mutated fragment F1
+pfy1_f1_mutated = HO_locus[-30:] + PFY1[0:144] # 124 to 144 are bases for the homology arm
+# Wild-Type fragment F1
+pfy1_f1_WT = PFY1[144-20:] + YFP_locus[:30]
+
+# Mutated fragment F2
+pfy1_f2_mutated = PFY1[(145-20-20):]
+# Wild-type fragment F1
+pfy1_f2_WT = PFY1[0:144-20]
+
+PFY1_promoter = {"PFY1_F1" : "AGGAGACGTTACTTTGTTTATATATATTAGTATGTACAATCGCAAAGAAATGGAGTGATGACATGTTGTAGTATTTAGTATGAGGTTACTGTGTGGGAGGTTTTACCATGATTTTTGGCGAGAACACGCCATGAAATGTCTTTG", 
+                 "PFY1_F2" : "CCATGATTTTTGGCGAGAACACGCCATGAAATGTCTTTGTACGAAACTCATTACCCGCATTAATATTTTTTTTCTTTTTAAAGCTCAGTTGACCCTTTCTCATTCCCTTCTTAAAACAACTGTGTGATCCTTGAGAAAAGATAAATTACATACACAACATAAACCCAACTACGATCGCAAATT"}
+
+PFY1_mutated_promoters = mutate_promoter(PFY1_promoter)
+pfy1_mutated_oligs = open("mutated_pfy1.txt", "w")
+for sequence in PFY1_mutated_promoters.keys():
+    if sequence.split("_")[1] == "F1":
+        pfy1_mutated_oligs.write(sequence+"\t"+HO_locus[-30:]+PFY1_mutated_promoters[sequence]+"\n")
+    elif sequence.split("_")[1] == "F2":
+        pfy1_mutated_oligs.write(sequence+"\t"+PFY1_mutated_promoters[sequence]+YFP_locus[:30]+"\n")
+
 #*********************************************************************************************
 def scan_recog_motif(mutated_promoter_dict, RE):
     """
@@ -97,26 +279,44 @@ def scan_recog_motif(mutated_promoter_dict, RE):
             print("No recognition site was detected")
     return(recog_dict)
 #*********************************************************************************************
-def split_promoter(mutated_promoter_dict, recog_dict, oligo_len):
+def split_promoter(mutated_promoter_dict, recog_dict, fixed_len = True, oligo_len, start, max_len, min_len):
     """
     Function to split the promoter sequence into fragments
+    We can split the promoter into fixed fragment length but strict length size could not be useful since
+    the sequence might not be optimal in terms of the future assembly method. It is best to specify a minimum and maximum
+    for the length of the fragment with a start position and then check the sequence for compatibility for recognition site.
+    Also evaluate   
     """
     # note!: Adjust the script for when one of the pieces is very short.
     mutated_fragment_dict = {}
-    for mutation in mutated_promoter_dict.keys():
-        if not mutation in recog_dict.keys():
-            fragments = chunks(mutated_promoter_dict[mutation], oligo_len)
-            for index, item in enumerate(fragments):
-                # Think about few more bases
-                #fragment_dict = {"fragment1" : RNR1[0:237], "fragment2" : RNR1[237-4:2*237], "fragment3" : RNR1[(2*237)-4:3*237], "fragment4" : RNR1[(3*237)-4:4*237]}
-                fragment_ID = mutation + "_F" + str(index+1)
-                mutated_fragment_dict[fragment_ID] = item
+    if fixed_len:
+        for mutation in mutated_promoter_dict.keys():
+            if not mutation in recog_dict.keys():
+                fragments = chunks(mutated_promoter_dict[mutation], oligo_len)
+                for index, item in enumerate(fragments):
+                    # Think about few more bases
+                    #fragment_dict = {"fragment1" : RNR1[0:237], "fragment2" : RNR1[237-4:2*237], "fragment3" : RNR1[(2*237)-4:3*237], "fragment4" : RNR1[(3*237)-4:4*237]}
+                    fragment_ID = mutation + "_F" + str(index+1)
+                    mutated_fragment_dict[fragment_ID] = item
+    else:
+        min_end = start+min_len
+        max_end = start+max_len
+        for end in range(min_end, max_end):
+            new_oligo_len = 
+        oligo_range = [[start, min_end], [start, max_end]]
+
     return(mutated_fragment_dict)
 #*********************************************************************************************
-def oligo_fragments(mutated_fragment_dict):
+# def oligo_fragments(mutated_fragment_dict):
 
+#*********************************************************************************************
+# Split promoter into mutated fragment and wildtype fragment
+#*********************************************************************************************
 
-
+# Gibson assembly is a protocol for joining DNA fragments in-vitro by tratment with a mixture of T5 exconuclease,
+# DNA polymerase and Taq DNA ligase. Gibson assembly requires 20-40 bp of perfect homology between 3' and 5' ends for 
+# fragments to be joined. The T5 exonuclease chews back each fragment in the 5'-3' direction so that the remaining 3' 
+# single stranded overhangs can anneal. Gaps are filled and nicks sealed by polymerase and ligase. 
 
 # The first and last ligo must produce homology with the plasmid sequence after it is digested by BsaI
 oligo1 = 20*"N" + "GGTCTC" + "N" + "ACAT" + fragment1 + "N" + "GAGACC" + 20*"N"
