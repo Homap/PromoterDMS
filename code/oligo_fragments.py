fa17f711b63ee4def2dcee3c6316149f64a37a32
homap <hpapoli@gmail.com>
2023-05-31 23:25:12 -0400

    function for fragment design

--- code/oligo_fragments.py
+++ code/oligo_fragments.py
@@ -48,71 +48,74 @@ def oligo_design(promoter_sequence, fragment_len=150):
     mutated_oligo_dict = {}
     WT_oligo_dict = {}
     for oligo in range(1, num_oligo_fragments+1):
-        if oligo == 1:
-            first_fragment = promoter_sequence[0:fragment_len+20] # HO_locus[-30:]
-            key = "oligo_" + str(oligo)
-            mutated_oligo_dict[key] = first_fragment
+        if oligo == 1: # Each oligo contains overlap and the region to be mutated
+            #first_fragment = promoter_sequence[0:fragment_len+20] # HO_locus[-30:]
+            first_fragment = promoter_sequence[0:fragment_len]
+            first_fragment_overlap = promoter_sequence[fragment_len:fragment_len+20]
+            key_m = "oligo_" + str(oligo) + "_M"
+            key_w = "oligo_" + str(oligo) + "_W"
+            mutated_oligo_dict[key_m] = {"HO_arm": HO_locus[-30:], "oligo": first_fragment, "overlap": first_fragment_overlap}
             first_fragment_WT = promoter_sequence[fragment_len:len(promoter_sequence)]
-            WT_oligo_dict[key] = first_fragment_WT
+            WT_oligo_dict[key_w] = {"oligo": first_fragment_WT, "YFP_arm": YFP_locus[:30]}
         elif oligo == num_oligo_fragments:
-            last_fragment = promoter_sequence[((num_oligo_fragments-1)*fragment_len)-20:len(promoter_sequence)] # YFP_locus[:30]
-            key = "oligo_" + str(oligo)
-            mutated_oligo_dict[key] = last_fragment
+            #last_fragment = promoter_sequence[((num_oligo_fragments-1)*fragment_len)-20:len(promoter_sequence)] # YFP_locus[:30]
+            last_fragment = promoter_sequence[((num_oligo_fragments-1)*fragment_len):len(promoter_sequence)]
+            last_fragment_overlap = promoter_sequence[((num_oligo_fragments-1)*fragment_len)-20:((num_oligo_fragments-1)*fragment_len)]
+            key_m = "oligo_" + str(oligo) + "_M"
+            key_w = "oligo_" + str(oligo) + "_W"
+            mutated_oligo_dict[key_m] = {"overlap": last_fragment_overlap, "oligo": last_fragment, "YFP_arm": YFP_locus[:30]}
             last_fragment_WT = promoter_sequence[0:((num_oligo_fragments-1)*fragment_len)]
-            WT_oligo_dict[key] = last_fragment_WT
+            WT_oligo_dict[key_w] = {"HO_arm": HO_locus[-30:], "oligo": last_fragment_WT}
         else:
             if num_oligo_fragments > 2:
                 mid_fragment = promoter_sequence[((oligo-1)*fragment_len)-20:(oligo*fragment_len)+20]
-                key = "oligo_" + str(oligo)
-                mutated_oligo_dict[key] = mid_fragment
+                mid_fragment_overlap1 = promoter_sequence[((oligo-1)*fragment_len)-20:((oligo-1)*fragment_len)]
+                mid_fragment_overlap2 = promoter_sequence[(oligo*fragment_len):(oligo*fragment_len)+20]
+                key_m = "oligo_" + str(oligo) + "_M"
+                key_w = "oligo_" + str(oligo) + "_W"
+                mutated_oligo_dict[key_m] = {"overlap1": mid_fragment_overlap1, "oligo": mid_fragment, "overlap2": mid_fragment_overlap2}
                 mid_fragment_WT_1 = promoter_sequence[0:(oligo-1)*fragment_len]
                 mid_fragment_WT_2 = promoter_sequence[oligo*fragment_len:len(promoter_sequence)]
-                WT_oligo_dict[key] = [mid_fragment_WT_1, mid_fragment_WT_2]
+                WT_oligo_dict[key_w] = {"HO_arm": HO_locus[-30:], "oligo_1": mid_fragment_WT_1,"oligo_2": mid_fragment_WT_2, "YFP_arm": YFP_locus[:30]}
             else:
                 pass
-    return([mutated_oligo_dict, WT_oligo_dict])
+    return([{"mutated": mutated_oligo_dict}, {"wildtype": WT_oligo_dict}])
 #*********************************************************************************************
-def mutate_promoter(promoter):
+def mutate_oligo(oligo_seq):
     """
     Function to produce 1 mutation to each of three possibile bases
     on the promoter sequence
     """
-    #promoter_dict = readfasta(promoter) # read promoter fasta into a dictionary
-    promoter_dict = promoter
-    mutated_promoter_dict = {}
-    for gene_name in promoter_dict.keys():
-        #for position in range(0, len(promoter_dict[gene_name])):
-        for index, item in enumerate(list(promoter_dict[gene_name])):
-            nuc = promoter_dict[gene_name][index]
-            if nuc == 'A':
-                for base in nuc_dict['A']:
-                    mutated_seq_key = gene_name + "_" + str(index+1) + "_" + nuc + base
-                    promoter_base_list = list(promoter_dict[gene_name])
-                    promoter_base_list[index] = base
-                    mutated_seq = "".join(promoter_base_list)
-                    mutated_promoter_dict[mutated_seq_key] = mutated_seq
-            if nuc == 'T':
-                for base in nuc_dict['T']:
-                    mutated_seq_key = gene_name + "_" + str(index+1) + "_" + nuc + base
-                    promoter_base_list = list(promoter_dict[gene_name])
-                    promoter_base_list[index] = base
-                    mutated_seq = "".join(promoter_base_list)
-                    mutated_promoter_dict[mutated_seq_key] = mutated_seq
-            if nuc == 'C':
-                for base in nuc_dict['C']:
-                    mutated_seq_key = gene_name + "_" + str(index+1) + "_" + nuc + base
-                    promoter_base_list = list(promoter_dict[gene_name])
-                    promoter_base_list[index] = base
-                    mutated_seq = "".join(promoter_base_list)
-                    mutated_promoter_dict[mutated_seq_key] = mutated_seq
-            if nuc == 'G':
-                for base in nuc_dict['G']:
-                    mutated_seq_key = gene_name + "_" + str(index+1) + "_" + nuc + base
-                    promoter_base_list = list(promoter_dict[gene_name])
-                    promoter_base_list[index] = base
-                    mutated_seq = "".join(promoter_base_list)
-                    mutated_promoter_dict[mutated_seq_key] = mutated_seq
-    return(mutated_promoter_dict)
+    mutated_promoter_list = []
+    for index, item in enumerate(list(oligo_seq)):
+        nuc = list(oligo_seq)[index]
+        if nuc == 'A':
+            for base in nuc_dict['A']:
+                promoter_base_list = list(oligo_seq)
+                promoter_base_list[index] = base
+                mutated_seq = "".join(promoter_base_list)
+                mutated_promoter_list.append(mutated_seq)
+        elif nuc == 'T':
+            for base in nuc_dict['T']:
+                promoter_base_list = list(oligo_seq)
+                promoter_base_list[index] = base
+                mutated_seq = "".join(promoter_base_list)
+                mutated_promoter_list.append(mutated_seq)
+        elif nuc == 'C':
+            for base in nuc_dict['C']:
+                promoter_base_list = list(oligo_seq)
+                promoter_base_list[index] = base
+                mutated_seq = "".join(promoter_base_list)
+                mutated_promoter_list.append(mutated_seq)
+        elif nuc == 'G':
+            for base in nuc_dict['G']:
+                promoter_base_list = list(oligo_seq)
+                promoter_base_list[index] = base
+                mutated_seq = "".join(promoter_base_list)
+                mutated_promoter_list.append(mutated_seq)
+        else:
+            print("Error: Illigal character")
+    return(mutated_promoter_list)
 #*********************************************************************************************
 
 # Gibson assembly is a protocol for joining DNA fragments in-vitro by tratment with a mixture of T5 exconuclease,
